946fdc90491fdfbf5d89e9a395eda0e7d2c1c847
adam brunson <adam.brunson@yahoo.com>
2025-02-10 04:40:05 -0800

    update css .card & layout for about me

---
+++ cards.js
@@ -0,0 +1,12 @@
+    <div class="container">
+      <div class="card-bio">
+        <img class="img-circle" src="images/adam_brun.png" width="200px" alt="Adam Brunson"/>
+        <h1 style="margin-bottom: 0%;">
+          Adam Brunson
+          <h2 style="margin-top: 0%;">Jr.Developer and Professional Motion Graphics Artist</h2>
+        </h1>
+      </div>
+      <div class="card-bio">
+        <p> Hello, everyone I like to code and do video editing.</p>
+      </div>
+    </div>
